017194246517a7ca1d51ef1b77b567afd321a509
Robin\RobinThuan <robinthuan12345678@gmail.com>
2017-12-12 13:02:26 +0700

    no message

--- app/src/main/java/com/example/administrator/mvp_demo/MainActivity.java
+++ app/src/main/java/com/example/administrator/mvp_demo/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.administrator.mvp_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.example.administrator.mvp_demo.mvp.adapter.RecylerAdapter;
 import com.example.administrator.mvp_demo.mvp.model.ListPeopleHelper;
 import com.example.administrator.mvp_demo.mvp.model.OnloadPeopleResult;
 import com.example.administrator.mvp_demo.mvp.model.People;
@@ -12,35 +16,66 @@ import com.example.administrator.mvp_demo.mvp.view.ListPeopeleView;
 
 import org.json.JSONException;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ListPeopeleView {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements ListPeopeleView, RecylerAdapter.OnResultClickListener {
     ListPeopleHelper listPeopleHelper;
     ListPeoplePresent listPeoplePresent;
+    ArrayList<People> list;
+    @BindView(R.id.recylerView)
+    RecyclerView recyclerView;
+    RecylerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listPeopleHelper=new ListPeopleHelper();
+        ButterKnife.bind(this);
+        list = new ArrayList<>();
+        listPeopleHelper = new ListPeopleHelper();
 
-    Thread thread=new Thread(new Runnable() {
-        @Override
-        public void run() {
-        String respone=listPeopleHelper.getPeoplefromServer("https://api.randomuser.me/?results=1&nat=en");
-            try {
-                listPeopleHelper.parseJson(respone);
-            } catch (JSONException e) {
-                e.printStackTrace();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String respone = listPeopleHelper.getPeoplefromServer("https://api.randomuser.me/?results=50&nat=en");
+                try {
+                    listPeopleHelper.parseJson(respone);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                listPeopleHelper.getListPeople();
             }
-            listPeopleHelper.getListPeople();        }
-    });
-    thread.start();
-        listPeoplePresent=new ListPeoplePresent(this,listPeopleHelper);
+        });
+        thread.start();
+        listPeoplePresent = new ListPeoplePresent(this, listPeopleHelper);
+
+
     }
 
 
     @Override
     public void displaylistPeople(List<People> peopleList) {
-        Log.d("Phone:", "displaylistPeople: "+peopleList.get(0).getPhone());
+
+        list = (ArrayList<People>) peopleList;
+        adapter = new RecylerAdapter(list, this);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+            }
+        });
+
+    }
+
+    @Override
+    public void clickListener(int positon) {
+
     }
 }
--- app/src/main/java/com/example/administrator/mvp_demo/mvp/adapter/RecylerAdapter.java
+++ app/src/main/java/com/example/administrator/mvp_demo/mvp/adapter/RecylerAdapter.java
@@ -1,34 +1,61 @@
 package com.example.administrator.mvp_demo.mvp.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.mvp_demo.R;
+import com.example.administrator.mvp_demo.mvp.model.People;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by administrator on 12/8/17.
  */
 
 public class RecylerAdapter extends RecyclerView.Adapter<RecylerAdapter.Viewholder> {
-
+    ArrayList<People> list;
+    Context context;
+    OnResultClickListener onResultClickListener;
+    public RecylerAdapter(ArrayList<People> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+    public void attach(OnResultClickListener onResultClickListener){
+        this.onResultClickListener=onResultClickListener;
+    }
     @Override
     public Viewholder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.row_item,parent,false);
+        Viewholder viewholder=new Viewholder(view);
+        return viewholder;
     }
 
     @Override
-    public void onBindViewHolder(Viewholder holder, int position) {
-
+    public void onBindViewHolder(Viewholder holder, final int position) {
+        Picasso.with(context).load(list.get(position).getPicture().getMedium()).into(holder.icon);
+        holder.gender.setText(list.get(position).getGender().toString());
+        holder.name.setText(list.get(position).getName().getTitle() +" "+ list.get(position).getName().getFirst().toString());
+        holder.phone.setText(list.get(position).getPhone().toString());
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onResultClickListener.clickListener(position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class Viewholder extends RecyclerView.ViewHolder {
@@ -40,8 +67,14 @@ public class RecylerAdapter extends RecyclerView.Adapter<RecylerAdapter.Viewhold
         TextView gender;
         @BindView(R.id.phone)
         TextView phone;
+        View view;
         public Viewholder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this,itemView);
+            view=itemView;
         }
+    }
+     public interface  OnResultClickListener{
+        void clickListener(int positon);
     }
 }
